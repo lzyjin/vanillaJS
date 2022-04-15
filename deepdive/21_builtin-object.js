6a3e9381b105a84cb308d3f4591c425a87af29be
lzyjin <1247y.j@gmail.com>
2022-04-15 09:32:00 +0900

    빌트인객체

--- deepdive/21_builtin-object.js
+++ deepdive/21_builtin-object.js
@@ -134,6 +134,8 @@ console.log(date, typeof date); // Mon Mar 21 2022 21:52:00 GMT+0900 (한국 표
 
 // ❕문자열, 숫자, 불리언, 심벌 이외의 원시값, 즉 null과 undefined는 래퍼 객체를 생성하지 않는다. 따라서 null과 undefined 값을 객체처럼 사용하면 에러가 발생한다. 
 
+
+
 // 👉 21.4 전역 객체
 // 전역 객체(global object)는 코드가 실행되기 이전 단계에 자바스크립트 엔진에 의해 어떤 객체보다도 먼저 생성되는 특수한 객체이며, 어떤 객체에도 속하지 않은 최상위 객체다.
 // 전역 객체는 자바스크립트 환경에 따라 지칭하는 이름이 제각각이다.
@@ -334,4 +336,284 @@ console.clear();
   console.log( isFinite(null) ); // true (null -> 0)
 
   // 인수가 무한수 또는 NaN으로 평가되는 값이라면 false를 반환한다. 
+  console.log( isFinite(Infinity) ); // false
+  console.log( isFinite(-Infinity) ); // false
+
+  // 인수가 NaN으로 평가되는 값이라면 false를 반환한다.
+  console.log( isFinite(NaN) ); // false
+  console.log( isFinite('Hello') ); // false
+  console.log( isFinite('2005/12/12') ); // false
+}
+// isFinite(null)은 true를 반환한다. 이것은 null을 숫자로 변환하여 검사를 수행했기 때문이다. null을 숫자 타입으로 변환하면 0이 된다.
+console.log(+null); // 0
+
+// - isNaN
+{
+  // 숫자
+  console.log( NaN ); // NaN
+  console.log( 10 ); // 10
+
+  // 문자열
+  console.log( isNaN('blabla') ); // true
+  console.log( isNaN('10') ); // false ('10' -> 10)
+  console.log( isNaN('10.12') ); // false ('10.12' -> 10.12)
+  console.log( isNaN('') ); // false ('' -> 0)
+  console.log( isNaN(' ') ); // false (' ' -> 0)
+
+  // 불리언
+  console.log( isNaN(true) ); // false ( true -> 1)
+  console.log( isNaN(null) ); // false (null -> 0)
+
+  // undefined
+  console.log( isNaN(undefined) ); // true (undefined -> NaN)
+
+  // 객체
+  console.log( isNaN({}) ); // true ({} -> NaN)
+
+  // date
+  console.log( isNaN(new Date()) ); // false (new Date() -> Number)
+  console.log( isNaN(new Date().toString()) ); // true (String -> NaN)
+}
+
+// - parseFloat
+// 전달 받은 문자열 인수를 부동 소수점 숫자(floating point number), 즉 실수로 해석(parsing)하여 반환한다.
+{
+  // 문자열을 실수로 해석하여 반환한다.
+  console.log( parseFloat('3.14') ); // 3.14
+  console.log( parseFloat('10.00') ); // 10
+
+  // ❕ 공백으로 구부된 문자열은 첫 번째 문자열만 반환한다.
+  console.log( parseFloat('34 45 66') ); // 34
+  console.log( parseFloat('40 years') ); // 40
+
+  // 첫 번째 문자열을 숫자로 변환할 수 없다면 NaN을 반환한다.
+  console.log( parseFloat('He was 40') ); // NaN
+}
+
+// parseInt
+// 전달받은 문자열 인수를 정수integer로 해석parsing하여 반환한다.
+{
+  console.log( parseInt('10') ); // 10
+  console.log( parseInt('10.123') ); // 10
+
+  // 전달받은 인수가 문자열이 아니면 문자열로 변환한 다음, 정수로 해석하여 반환한다.
+  console.log( parseInt(10) ); // 10
+  console.log( parseInt(10.123) ); // 10
+
+  // 두 번째 인수로 진법을 나타내는 기수(2~36)을 전달할 수 있다.
+  // 기수를 지정하면 첫 번째 인수로 전달된 문자열을 해당 기수의 숫자로 해석하여 반환한다.
+  // 이때 반환값은 언제나 10진수다.
+  // 기수를 생략하면 첫 번째 인수로 전달된 문자열을 10진수로 해석하여 반환한다.
+  console.log( parseInt('10') ); // 10
+  console.log( parseInt('10', 2) ); // 2
+  console.log( parseInt('10', 8) ); // 8
+  console.log( parseInt('10', 16) ); // 16
+
+  // 참고로 기수를 지정하며 10진수 숫자를 해당 기수의 문자열로 변환하여 반환하고 싶을 때는 Number.prototype.toString 메서드를 사용한다.
+  const x = 15;
+
+  // 10진수 15를 2진수로 변환하여 그 결과를 문자열로 반환한다
+  console.log( x.toString(2) ); //1111
+  // 문자열 '1111'을 2진수로 해석하고 그 결과를 10진수 정수로 반환한다.
+  console.log( parseInt(x.toString(2), 2) );
+
+  // 10진수 15를 8진수로 변환하여 그 결과를 문자열로 반환한다.
+  console.log( x.toString(8) ); // 17
+  // 문자열 '17'을 8진수로 해석하고 그 결과를 10진수 정수로 반환한다.
+  console.log( parseInt(x.toString(8), 8) ); // 15
+
+  // 10진수 15를 16진수로 변환하여 그 결과를 문자열로 반환한다.
+  console.log( x.toString(16) ); // f
+  // 문자열 'f'을 8진수로 해석하고 그 결과를 10진수 정수로 반환한다.
+  console.log( parseInt(x.toString(16), 16) ); // 15
+
+  console.log( x.toString() ); // '15'
+  console.log( parseInt(x.toString()) ); // 15
+
+  
+  // 두번째 인수로 진법을 나타내는 기수를 지정하지 않더라도 첫 번째 인수로 전달된 문자열이 "0x" 또는 "0X"로 시작하는 16진수 리터럴이라면 16진수로 해석하여 10진수 정수로 반환한다.
+  console.log( parseInt('0xf') ); // 15
+  // 위코드와 같다
+  console.log( parseInt('f', 16) ); // 15
+
+
+  // 하지만 2진수 리터럴과 8진수 리터럴은 제대로 해석하지 못한다. 0 이후가 무시된다.
+  console.log( parseInt('0b10') ); // 0
+  // 8진수 리터럴(ES6에서 도입. 0o로 시작)은 제대로 해석하지 못한다. 0 이후가 무시된다.
+  console.log( parseInt('0o10') ); // 0
+
+  // ES5 이전까지는 비록 사용을 금지하고는 있었지만 "0"으로 시작하는 숫자르 8진수로 해석했다.
+  // ES6부터는 "0"으로 시작하는 숫자를 8진수로 해석하지 않고 10진수로 해석한다.
+  // 따라서 문자열을 8진수로 해석하려며 지수를 반드시 지정해야 한다.
+
+  // 문자열 '10'을 2진수로 해석한다
+  console.log( parseInt('10', 2) ); // 2
+  // 문자열 '10'을 8진수로 해석한다
+  console.log( parseInt('10', 8) ); // 8
+
+
+  // 첫 번째 인수로 전달한 문자열의 첫 번째 문자가 해당 지수의 숫자로 변환될 수 없다면 NaN을 반환한다.
+  // 'A'는 10진수로 해석할 수 없다
+  console.log( parseInt('A0') ); // NaN
+  // '2'는 2진수로 해석할 수 없다.
+  console.log( parseInt('20', 2) ); // NaN
+
+
+  // 하지만 첫 번째 인수로 전달한 문자열의 두번째 문자부터 해당 진수를 나타내는 문자가 아닌 문자(예를 들어 2진수의 경우 2)와 마주치면 이 문자와 계속되는 문자들은 전부 무시되며 해석된 정수값만 반환한다.
+  // 10진수로 해석할 수 없는 'A' 이후의 문자는 모두 무시된다.
+  console.log( parseInt('1A0') ); // 1
+  // 2진수로 해석할 수 없는 '2' 이후의 문자는 모두 무시된다.
+  console.log( parseInt('102', 2) ); // 2
+  // 8진수로 해석할 수 없는 '8' 이후의 문자는 모두 무시된다.
+  console.log( parseInt('58', 8) ); // 5
+  // 16진ㅅ로 해석할 수 없는 'G' 이후의 문자는 모두 무시된다.
+  console.log( parseInt('FG', 16) ); // 15
+
+
+  // 첫 번째 인수로 전달한 문자열에 공백이 있다면 첫 번째 문자열만 해석하여 반환하며 앞뒤 공백은 무시된다.
+  // 만일 첫 번째 문자열을 숫자로 해석할 수 없는 경우 NaN을 반환한다.
+  console.log( parseInt('34 45 66') ); // 34
+  console.log( parseInt('40 years') ); // 40
+  // 첫번째 문자열을 숫자로 변환할 수 없다면 NaN을 반환한다.
+  console.log( parseInt('He was 20') ); // NaN
+  // 앞뒤 공백은 무시된다.
+  console.log( parseInt(' 50 ') ); //50
+}
+
+
+
+// encodeURI / decodeURI
+// encodeURI 함수는 완전한 URI(Uniform Resource Identifier)를 문자열로 전달받아 이스케이프 처리를 위해 인코딩한다.
+// URI는 인터넷에 있는 자원을 나타내는 유일한 주소를 말한다. URI의 하위개념으로 URL, URN이 있다.
+
+// 인코딩이란 URI의 문자들을 이스케이프 처리하는 것을 의미한다.
+// 이스케이프 처리는 네트워크를 통해 정보를 공유할 때 어떤 시스템에서도 읽을 수 있는 아스키 문자 셋으로 변환하는 것이다. 
+// UTF-8 특수 문자의 경우 1문자 당 1~3 바이트 UTF-8 한글 표현의 경우 1문자당 3바이트다. 
+// 예를들어, 특수 문자인 공백 문자는 %20, 한글 '가'는 %EC%9E%90으로 인코딩된다.
+
+// URI 문법 형식 표준 FRC3986에 따르면 URL은 아스키 문자 셋으로만 구성되어야 하며 한글을 포함한 대부분의 외국어나 아스키 문자 셋에 정의되지 않은 특수 문자의 경우 URL에 포함될 수 없다.
+// 따라서 URL 내에서 의미를 갖고 있는 문자(%, ?, #)나 URL에 올 수 없는 문자(한글,공백) 또는 시스템에 의해 해석될 수 있는 문자(<, >)를 이스케이프 처리하여 야기될 수 있는 문제를 예방하기 위해 이스케이프 처리가 필요하다.
+// 단, 알파벳, 0~9의 숫자, - _ . ! ~ * ' ( ) 문자는 이스케이프 처리에서 제외된다.
+{
+  // 완전한 URI
+  const uri = 'http://example.com?name=이응모&job=programmer&teacher';
+  
+  // encodeURI 함수는 완전한 URI를 전달받아 이스케이프 처리를 위해 인코딩한다.
+  const enc = encodeURI(uri);
+  console.log(enc); // http://example.com?name=%EC%9D%B4%EC%9D%91%EB%AA%A8&job=programmer&teacher
+
+}
+
+// decodeURI 함수는 인코딩된 URI를 인수로 전달받아 이스케이프 처리 이전으로 디코딩한다.
+{
+  // 완전한 URI
+  const uri = 'http://example.com?name=이응모&job=programmer&teacher';
+  
+  // encodeURI 함수는 완전한 URI를 전달받아 이스케이프 처리를 위해 인코딩한다.
+  const enc = encodeURI(uri);
+  console.log(enc); // http://example.com?name=%EC%9D%B4%EC%9D%91%EB%AA%A8&job=programmer&teacher
+
+  // decodeURI 함수는 인코딩된 URI를 인수로 전달받아 이스케이프 처리 이전으로 디코딩한다.
+  const dec = decodeURI(enc);
+  console.log(dec); // http://example.com?name=이응모&job=programmer&teacher
+}
+
+
+// encodeURIComponent / decodeURIComponent 
+// encodeURIComponent 함수는 URI의 구성요소(component)를 인수로 전달받아 인코딩한다.
+// 인코딩: URI의 문자들을 이스케이프 처리하는 것
+// decodeURIComponent 함수는 매개변수로 전달된 URI 구성요소를 디코딩한다 
+
+// ❕ encodeURIComponent 함수는 인수로 전달된 문자열을 URI의 구성요소인 쿼리 스트링의 일부로 간주한다.
+// 따라서 쿼리 스트링 구분자로 사용되는 =, ?, & 까지 인코딩한다.
+// 반면 encodeURI 함수는 매개변수로 전달된 문자열을 완전한 URI 전체라고 간주한다. 
+// 따라서 쿼리 스트링 구분자로 사용되는 =, ?, &은 인코딩하지 않는다. 
+{
+  // URI의 쿼리스트링
+  const uriComp = 'name=이응모&job=programmer&teacher';
+
+  // encodeURIComponent 함수는 인수로 전달된 문자열을 URI의 구성요소인 쿼리 스트링의 일부로 간주한다.
+  // 따라서 쿼리 스트링 구분자로 사용되는 =, ?, & 까지 인코딩한다.
+  let enc = encodeURIComponent(uriComp);
+  console.log(enc); // name%3D%EC%9D%B4%EC%9D%91%EB%AA%A8%26job%3Dprogrammer%26teacher
+
+  let dec = decodeURIComponent(enc);
+  console.log(dec); // name=이응모&job=programmer&teacher
+
+  // 반면 encodeURI 함수는 매개변수로 전달된 문자열을 완전한 URI 전체라고 간주한다. 
+  // 따라서 쿼리 스트링 구분자로 사용되는 =, ?, &은 인코딩하지 않는다. 
+  enc = encodeURI(uriComp);
+  console.log(enc); // name=%EC%9D%B4%EC%9D%91%EB%AA%A8&job=programmer&teacher
+
+  dec = decodeURI(enc);
+  console.log(dec); // name=이응모&job=programmer&teacher
+}
+
+// 📌 21.4.3 암묵적 전역
+{
+  var x = 10; // 전역 변수
+  
+  function foo() {
+    // 선언하지 않은 식별자에 값을 할당
+    y = 20; // window.y = 20;
+  }
+  foo();
+
+  // 선언하지 않은 식별자 y를 전역에서 참조할 수 있다.
+  console.log( x + y ); // 30
+}
+// foo 함수 내의 y는 선언하지 않은 식별자다.
+// 따라서 y = 20이 실행되면 참조 에러가 발생할 것처럼 보인다. 
+// 하지만 선언하지 않은 식별자 y는 마치 선언된 전역 변수처럼 동작한다.
+// 이는 선언하지 않은 식별자에 값을 할당하면 전역 객체의 프로퍼티가 되기 때문이다.
+
+// foo 함수가 호출되면 자바스크립트 엔진은 y 변수에 값을 할당하기 위해 먼저 스코프 체인을 통해 선언된 변수인지 확인한다.
+// 이때 foo 함수의 스코프와 전역 스코프 어디에서도 y 변수의 선언을 찾을 수 없으므로 참조 에러가 발생한다.
+// 하지만 자바스크립트 엔진은 y = 20;을 window.y = 20으로 해석하여 전역 객체에 프로퍼티를 동적 생성한다.
+// 결국 y는 전역 객체의 프로퍼티가 되어 마치 전역 변수처럼 동작한다. 
+// ❕ 이러한 현상을 암묵적 전역(implict global)이라 한다.
+
+// 하지만 y는 변수 선언 없이 단지 전역 객체의 프로퍼티로 추가되었을 뿐이다.
+// 따라서 y는 변수가 아니다.
+// y는 변수가 아니므로 변수 호이스팅이 발생하지 않는다.
+{
+
+  // ❕ 전역변수 x2는 호이스팅이 발생한다.
+  // console.log(x2); // undefined
+  // 전역 변수가 아니라 단지 전역 객체의 프로퍼티인 y는 호이스팅이 발생하지 않는다.
+  // console.log(y2); // ReferenceError: y2 is not defined
+
+  var x2 = 1; // 전역 변수
+  
+  function foo2() {
+    // 선언하지 않은 식별자에 값을 할당
+    y2 = 2; // window.y2 = 2;
+  }
+  foo2();
+
+  // 선언하지 않은 식별자 y2를 전역에서 참조할 수 있다.
+  console.log( x2 + y2 ); // 3
+}
+
+// 또한 변수가 아니라 단지 프로퍼티인 y는 delete 연산자로 삭제할 수 있다.
+// 전역 변수는 프로퍼티이지만 delete 연산자로 삭제할 수 없다.
+{
+  var x3 = 100; // 전역 변수
+  
+  function foo3() {
+    // 선언하지 않은 식별자에 값을 할당
+    y3 = 200; // window.y3 = 200;
+    console.log( x3 + y3 );
+  }
+  foo3(); // 300
+
+  console.log( window.x3 ); // 100
+  console.log( window.y3 ); // 200
+
+  delete x3; // 전역 변수는 삭제되지 않는다.
+  delete y3; // 프로퍼티는 삭제된다.
+
+  console.log( window.x3 ); // 100
+  console.log( window.y3 ); // undefined
+
 }
