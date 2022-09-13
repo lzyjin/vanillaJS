0b8419416d440a4e3d1fa6d2d54b204e04fea35f
lzyjin <1247y.j@gmail.com>
2022-09-13 22:46:24 +0900

    javascript deep dive - ajax

--- deepdive/43_ajax.js
+++ deepdive/43_ajax.js
@@ -236,12 +236,62 @@
 // 🌷 XMLHttpRequest 객체의 이벤트 핸들러 프로퍼티
 // 이벤트 핸들러 프로퍼티       | 설명
 // onreadystatechange 중요 | readyState 프로퍼티 값이 변경된 경우
-//
+// onloadstart            | HTTP 요청에 대한 응답을 받기 시작한 경우
+// onprogress             | HTTP 요청에 대한 응답을 받느 도중 주기적으로 발생
+// onabort                | abort 메서드에 의해 HTTP 요청이 중단된 경우
+// onerror 중요            | HTTP 요청에 에러가 발생한 경우
+// onload 중요             | HTTP 요청이 성공적으로 완료한 경우
+// ontimeout              | HTTP 요청 시간이 초과한 경우
+// onloadend              | HTTP 요청이 완료한 경우. HTTP 요청이 성공 또는 실패하면 발생
 
 // 🌷 XMLHttpRequest 객체의 메서드
+// 메서드                 | 설명
+// open 중요             | HTTP 요청 초기화
+// send 중요             | HTTP 요청 전송
+// abort 중요            | 이미 전송된 HTTP 요청 중단
+// setRequestHeader 중요 | 특정 HTTP 요청 헤더의 값을 설정
+// getRequestHeader     | 특정 HTTP 요청 헤더의 값을 문자열로 반환
+
 // 🌷 XMLHttpRequest 객체의 정적 프로퍼티
+// 정적 프로퍼티        | 값 | 설명
+// UNSENT           | 0 | open 메서드 호출 이전
+// OPENED           | 1 | open 메서드 호출 이후
+// HEADERS_RECEIVED | 2 | send 메서드 호출 이후
+// LOADING          | 3 | 서버 응답 중(응답 데이터 미완성 상태)
+// DONE             | 4 | 서버 응답 완료
+
 // 📌 HTTP 요청 전송
+// HTTP 요청을 전송하는 경우 다음 순서를 따른다.
+// 1. XMLHttpRequest.prototype.open 메서드로 HTTP 요청을 초기화한다.
+// 2. 필요에 따라 XMLHttpRequest.prototype.setRequestHeader 메서드로 특정 HTTP 요청의 헤더 값을 설정한다.
+// 3. XMLHttpRequest.prototype.send 메서드로 HTTP 요청을 전송한다.
+{
+    // XMLHttpRequest 객체 생성
+    const xhr = new XMLHttpRequest();
+
+    // HTTP 요청 초기화
+    xhr.open('GET', '/users');
+
+    // HTTP 요청 헤더 설정
+    // 클라이언트가 서버로 전송할 데이터의 MIME 타입 지정: json
+    xhr.setRequestHeader('content-type', 'application/json');
+
+    // HTTP 요청 전송
+    xhr.send();
+}
+
 // 🌷 XMLHttpRequest.prototype.open
+// open 메서드는 서버에 전송할 HTTP 요청을 초기화한다.
+// open 메서드를 호출하는 방법은 다음과 같다.
+// xhr.open(method, url[, async]);
+
+// 매개변수 | 설명
+// method | HTTP 요청 메서드("GET", "POST", "PUT", "DELETE" 등)
+// url    | HTTP 요청을 전송할 URL
+// async  | 비동기 요청 여부. 옵션으로 기본값은 true이며, 비동기 방식으로 동작한다.
+
+//
+
 // 🌷 XMLHttpRequest.prototype.send
 // 🌷 XMLHttpRequest.prototype.setRequestHeader
 // 📌 HTTP 응답 처리
